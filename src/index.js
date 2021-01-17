405612de62e068c855fc2983e5ca110eff70b8af
Lukasz Kowalik <kowalix007@gmail.com>
2021-01-17 22:06:54 +0000

    commit tree fd7710a6e7c0daab020a8c884fe7ba6da68386fa

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+fetch ("https://dog.ceo/api/breeds/image/random/4")
+ .then(response => response.json())
+ .then(response => {
+  const $div = document.querySelector("div") ;
+  const $img = document.createElement("img");
+  $img.src = response.message[0]
+  const $img1 = document.createElement("img");
+  $img1.src = response.message[1]
+  const $img2 = document.createElement("img");
+  $img2.src = response.message[2]
+  const $img3 = document.createElement("img");
+  $img3.src = response.message[3]
+  $div.append($img)
+  $div.append($img1)
+  $div.append($img2)
+  $div.append($img3)
+  })
+
+  const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+  fetch (breedUrl)
+   .then(response => response.json())
+   .then(response => {
+    const $ul = document.querySelector("ul") 
+    const $breed = document.createElement("breed")
+    $breed.src = response.message[0]
+    $ul.append($ul)
+  })
